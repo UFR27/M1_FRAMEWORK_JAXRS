729ba3add457565a04ce51846161963da300198e
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-10-21 13:05:39 +0200

    SS1 + AN Client

--- jaxrs-client/src/main/java/fr/pantheonsorbonne/ufr27/miage/App.java
+++ jaxrs-client/src/main/java/fr/pantheonsorbonne/ufr27/miage/App.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -25,6 +26,7 @@ public class App {
 	private static final Client client = ClientBuilder.newClient();
 	private static final WebTarget webTarget = client.target("http://localhost:8080/");
 	private List<AvailabilityNeutralResponse> currentProposals;
+	private List<AvailabilityNeutralResponse> ssed = new ArrayList<>();
 
 	public static void main(String[] args) {
 
@@ -36,38 +38,81 @@ public class App {
 	private void handleTTE(String tte) {
 		System.out.println("TTE");
 		System.out.println("tte:" + tte);
-		//PUT CODE HERE
+		// PUT CODE HERE
 
 	}
 
 	private void handleTTP(String ttp) {
 		System.out.println("TTP");
 		System.out.println("ttp:" + ttp);
-		//PUT CODE HERE
+		// PUT CODE HERE
 
 	}
 
 	private void handleTQT(String tqt) {
 		System.out.println("TQT");
 		System.out.println("tqt:" + tqt);
-		//PUT CODE HERE
+		// PUT CODE HERE
 
 	}
 
 	private void handleFXP() {
 		System.out.println("FXP");
-		//PUT CODE HERE
+		// PUT CODE HERE
 
 	}
 
 	private void handleSS(String count, String klass, String line) {
-		System.out.println("SS");
-		System.out.println("count:" + count);
-		System.out.println("klass:" + klass);
-		System.out.println("line:" + line);
-		//PUT CODE HERE
-		
-		
+		// System.out.println("SS");
+		// System.out.println("count:" + count);
+		// System.out.println("klass:" + klass);
+		// System.out.println("line:" + line);
+
+		AvailabilityNeutralResponse resp = currentProposals.get(Integer.parseInt(line));
+		AS as = new AS();
+		as.setKlass(klass);
+		as.setSeat(Integer.parseInt(count));
+
+		resp.getNbPlacesRestantes().getAS().clear();
+		resp.getNbPlacesRestantes().getAS().add(as);
+
+		WebTarget anWebTarget = webTarget.path("SS");
+		Response response = anWebTarget.request(MediaType.APPLICATION_XML_TYPE)
+				.post(Entity.entity(resp, MediaType.APPLICATION_XML_TYPE));
+
+		if (response.getStatus() == 200) {
+			System.out.println("ok");
+			
+			ssed.add(resp);
+			showSS();
+		} else {
+			System.out.print("seats are not available anymore...");
+		}
+
+	}
+
+	private void showSS() {
+		int i = 1;
+		// 1 AF 623 Y 04FEV 1 BDXCDG 1215 1325
+		for (AvailabilityNeutralResponse resp : this.ssed) {
+			String line = String.format("%d %2s %s\t%s\t%02d%02d %02d %s%s %02d%02d %02d%02d", //
+					i++, //
+					resp.getIdentifiantCompanie(), //
+					resp.getIdentifiantVol(), //
+					resp.getNbPlacesRestantes().getAS().get(0).getKlass(), // )
+					resp.getHeureDepart().getDay(), //
+					resp.getHeureDepart().getMonth(), //
+					resp.getNbPlacesRestantes().getAS().get(0).getSeat(), //
+					resp.getOrigine(), //
+					resp.getDestination(), //
+					resp.getHeureDepart().getHour(), //
+					resp.getHeureDepart().getMinute(), //
+					resp.getHeureArrivee().getHour(), //
+					resp.getHeureArrivee().getMinute());
+
+			System.out.println(line);
+
+		}
 
 	}
 
@@ -84,7 +129,7 @@ public class App {
 		AvailabilityNeutralResponses anResponses = (AvailabilityNeutralResponses) resp
 				.readEntity(AvailabilityNeutralResponses.class);
 
-		List<AvailabilityNeutralResponse> currentProposals = new ArrayList<>(anResponses.getResponses());
+		currentProposals = new ArrayList<>(anResponses.getResponses());
 
 		showAN(currentProposals);
 
@@ -109,80 +154,84 @@ public class App {
 
 	public void run() {
 		try (Scanner scanner = new Scanner(System.in)) {
+			try {
 
-			System.out.println("Enter the number here:");
-			while (!scanner.hasNextInt() && scanner.hasNext()) {
-				String input = scanner.next();
-				{
+				System.out.println("Enter the number here:");
+				while (!scanner.hasNextInt() && scanner.hasNext()) {
+					String input = scanner.next();
+					{
 
-					Pattern p = Pattern.compile("AN(.{5})(.{3})(.{3})(.{0,4})");
-					Matcher matcher = p.matcher(input);
-					if (matcher.matches()) {
-						String date = matcher.group(1);
-						String org = matcher.group(2);
-						String des = matcher.group(3);
-						String time = matcher.group(4);
+						Pattern p = Pattern.compile("AN(.{5})(.{3})(.{3})(.{0,4})");
+						Matcher matcher = p.matcher(input);
+						if (matcher.matches()) {
+							String date = matcher.group(1);
+							String org = matcher.group(2);
+							String des = matcher.group(3);
+							String time = matcher.group(4);
 
-						handleAN(date, org, des, time);
-						continue;
+							handleAN(date, org, des, time);
+							continue;
+						}
 					}
-				}
-				{
-					Pattern p = Pattern.compile("SS(\\d)([A-Z])(\\d)");
-					Matcher matcher = p.matcher(input);
-					if (matcher.matches()) {
-						String count = matcher.group(1);
-						String klass = matcher.group(2);
-						String line = matcher.group(3);
+					{
+						Pattern p = Pattern.compile("SS(\\d)([A-Z])(\\d)");
+						Matcher matcher = p.matcher(input);
+						if (matcher.matches()) {
+							String count = matcher.group(1);
+							String klass = matcher.group(2);
+							String line = matcher.group(3);
 
-						handleSS(count, klass, line);
-						continue;
+							handleSS(count, klass, line);
+							continue;
+						}
 					}
-				}
 
-				{
-					Pattern p = Pattern.compile("FXP");
-					Matcher matcher = p.matcher(input);
-					if (matcher.matches()) {
+					{
+						Pattern p = Pattern.compile("FXP");
+						Matcher matcher = p.matcher(input);
+						if (matcher.matches()) {
 
-						handleFXP();
-						continue;
+							handleFXP();
+							continue;
+						}
 					}
-				}
 
-				{
-					Pattern p = Pattern.compile("TQT/(.{8})");
-					Matcher matcher = p.matcher(input);
-					if (matcher.matches()) {
-						String tqt = matcher.group(1);
+					{
+						Pattern p = Pattern.compile("TQT/(.{8})");
+						Matcher matcher = p.matcher(input);
+						if (matcher.matches()) {
+							String tqt = matcher.group(1);
 
-						handleTQT(tqt);
-						continue;
+							handleTQT(tqt);
+							continue;
+						}
 					}
-				}
 
-				{
-					Pattern p = Pattern.compile("TTP");
-					Matcher matcher = p.matcher(input);
-					if (matcher.matches()) {
-						String ttp = matcher.group(1);
+					{
+						Pattern p = Pattern.compile("TTP");
+						Matcher matcher = p.matcher(input);
+						if (matcher.matches()) {
+							String ttp = matcher.group(1);
 
-						handleTTP(ttp);
-						continue;
+							handleTTP(ttp);
+							continue;
+						}
 					}
-				}
 
-				{
-					Pattern p = Pattern.compile("TTE/(.{8})");
-					Matcher matcher = p.matcher(input);
-					if (matcher.matches()) {
-						String tte = matcher.group(1);
+					{
+						Pattern p = Pattern.compile("TTE/(.{8})");
+						Matcher matcher = p.matcher(input);
+						if (matcher.matches()) {
+							String tte = matcher.group(1);
 
-						handleTTE(tte);
-						continue;
+							handleTTE(tte);
+							continue;
+						}
 					}
-				}
 
+				}
+			} catch (Throwable t) {
+				t.printStackTrace();
 			}
 		}
 	}
--- jaxrs-server/src/main/java/fr/pantheonsorbonne/ufr27/miage/endpoints/SimpleSellEndpoint.java
+++ jaxrs-server/src/main/java/fr/pantheonsorbonne/ufr27/miage/endpoints/SimpleSellEndpoint.java
@@ -3,14 +3,17 @@ package fr.pantheonsorbonne.ufr27.miage.endpoints;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import fr.pantheonsorbonne.ufr27.miage.ASs;
 import fr.pantheonsorbonne.ufr27.miage.ASs.AS;
 import fr.pantheonsorbonne.ufr27.miage.AvailabilityNeutralResponse;
+import fr.pantheonsorbonne.ufr27.miage.ObjectFactory;
 import fr.pantheonsorbonne.ufr27.miage.business.AmadeusBusiness;
 import fr.pantheonsorbonne.ufr27.miage.business.AmadeusBusinessRandomImpl;
 
@@ -20,9 +23,11 @@ public class SimpleSellEndpoint {
 	@Inject
 	AmadeusBusiness business;
 
-	@GET
+
+
+	@POST
 	@Consumes(value = { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public Response getSS(AvailabilityNeutralResponse anResp) {
+	public Response postSS(AvailabilityNeutralResponse anResp) {
 
 		AS as = anResp.getNbPlacesRestantes().getAS().get(0);
 		if (anResp.getNbPlacesRestantes().getAS().size() > 1) {
@@ -35,7 +40,6 @@ public class SimpleSellEndpoint {
 		} else {
 			return Response.status(410, "Seats are not available anymore").build();
 		}
-
 	}
 
 }
